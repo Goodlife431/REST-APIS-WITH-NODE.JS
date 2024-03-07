76da3cadbf782d436c1485c843aa1801a5ce9af2
Goodlife431 <oduwolejohn431@gmail.com>
2024-03-07 17:14:51 +0100

    update

--- nodeRestApis/src/index.ts
+++ nodeRestApis/src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -20,3 +21,10 @@ const server = http.createServer(app);
 server.listen(8080, ()=> {
     console.log("Server running on port http://localhost:8080/");
 })
+
+const MONGO_URL = 'mongodb+srv://Goodlife431:m1901318@cluster0.utxffat.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('connected', ()=> { console.log('Connected')});
+mongoose.connection.on('error', (error: Error) => console.log(error));
